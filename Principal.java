a3b06253cf38dc69a255b932b4ac0c9643b9e48c
ChechoGR <seagiraldori@gmail.com>
2024-11-04 13:41:40 -0500

    Extensión del menú

--- Principal.java
+++ Principal.java
@@ -4,50 +4,73 @@ import java.util.LinkedList;
 
 import javax.swing.JOptionPane;
 
-public class Principal{
+public class Principal {
     public static void main(String[] args) {
         Metodos m = new Metodos();
-        int seleccion_opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese una opción del menú: "));
-        switch (seleccion_opcion){
-            case 1: // Llenar lista estudiantes ingenieria
-                LinkedList<EstudianteIng> lista = new LinkedList<>();
-                lista = m.IngresarRegistro(lista); //Se guarda en lista para la persistencia de datos
-                m.MostrarRegistro(lista);
-            break;
-            case 2: // Llenar lista estudiantes diseño
-            break;
-            case 3: // Llenar lista computador
-            break;
-            case 4: // Llenar lista Tableta
-            break;
-            case 5: // Importar listas
-                int importar_lista = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese una opción para importar: "));
-                switch (importar_lista){
-                    case 1:
+        JOptionPane.showMessageDialog(null, "Bienvenido a la plataforma!");
+        JOptionPane.showMessageDialog(null,
+                "Seleccione una opción \n" + "Opción 1: Llenar lista estudiantes ingeniería\n" +
+                        "Opción 2: Llenar lista estudiantes diseño\n" +
+                        "Opción 3: Llenar lista de computadores\n" + "Opción 4: Llenar lista de tabletas\n"
+                        + "Opción 5: Importar lista\n" + "Opción 6: Exportar lista");
+        
+        LinkedList<EstudianteIng> lista = new LinkedList<>();
+        boolean bandera = true;
+        while (bandera) {
+            int seleccion_opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese una opción del menú: "));
+            switch (seleccion_opcion) {
+                case 1: // Llenar lista estudiantes ingenieria
+                    lista = m.IngresarRegistro(lista); // Se guarda en lista para la persistencia de datos
                     break;
-                    case 2:
+                case 2: // Llenar lista estudiantes diseño
                     break;
-                    case 3:
+                case 3: // Llenar lista computador
                     break;
-                }
-            break;
-            case 6: // Exportar listas
-            int exportar_lista = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese una opción para importar: "));
-                switch (exportar_lista){
-                    case 1:
+                case 4: // Llenar lista Tableta
                     break;
-                    case 2:
+                case 5: // Importar listas
+                    int importar_lista = Integer
+                            .parseInt(JOptionPane.showInputDialog(null, "Ingrese una opción para importar: "));
+                    switch (importar_lista) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                    }
                     break;
-                    case 3:
+                case 6: // Exportar listas
+                    int exportar_lista = Integer
+                            .parseInt(JOptionPane.showInputDialog(null, "Ingrese una opción para importar: "));
+                    switch (exportar_lista) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                    }
                     break;
-                }
-            break;
-            case 7:
-            break;
-            case 8:
-            break;
-            case 9:
-            break;
+                case 7: // Mostrar lista ingeniería
+                    m.MostrarRegistro(lista);
+                    break;
+                case 8: // Mostrar lista diseño
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    JOptionPane.showMessageDialog(null, "Finalizando el programa \n" + "Vuelva pronto!");
+                    bandera = false;
+                    
+                    break;
+            }
         }
     }
 }
