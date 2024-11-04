1b19a7cf175975ae5d4c17de9cd28ffdb30173f3
ChechoGR <seagiraldori@gmail.com>
2024-11-04 12:49:02 -0500

    Se añade validación para promedio

--- Metodos.java
+++ Metodos.java
@@ -30,8 +30,24 @@ public class Metodos {
                             "Error de entrada", JOptionPane.ERROR_MESSAGE);
                 }
             }
-            EI.setPromedio_acum1(
-                    Float.parseFloat(JOptionPane.showInputDialog("Ingrese su promedio").replace(",", ".")));
+            boolean ValidezPromedio = false;
+            while (!ValidezPromedio) {
+                String Verif_promedio = JOptionPane.showInputDialog(("Ingrese su promedio académico"));
+                if (Verif_promedio == null || Verif_promedio.trim().isEmpty()){
+                    JOptionPane.showMessageDialog(null, "Entrada no válida. Debe ingresar un número entre 0 y 5", "Error de entrada", JOptionPane.ERROR_MESSAGE);
+                    continue;
+                }
+                Verif_promedio = Verif_promedio.replace(",", ".");
+                try {
+                    EI.setPromedio_acum1(Float.parseFloat(Verif_promedio));
+                    ValidezPromedio = true;
+                }   catch (NumberFormatException e){
+                        JOptionPane.showMessageDialog(null, "Error: Debe ingresar un número válido.",
+                            "Error de entrada", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            //EI.setPromedio_acum1(
+                    //Float.parseFloat(JOptionPane.showInputDialog("Ingrese su promedio").replace(",", ".")));
             EI.setSerial(JOptionPane.showInputDialog("Ingrese el serial"));
             lista1.add(EI);
             int opt = JOptionPane.showConfirmDialog(null, "¿Desea ingresar otro registro?", "Continuar",
